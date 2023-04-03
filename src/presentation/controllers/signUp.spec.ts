e454c315ba4eff701cd811cd041276d3ba8a3fae
Thiago <morim240932gmail.com>
2023-04-03 18:43:26 -0300

    test: ensure signUp resturns new error if no email is provider

--- src/presentation/controllers/signUp.spec.ts
+++ src/presentation/controllers/signUp.spec.ts
@@ -13,5 +13,23 @@ describe('Sign Up Controller', () => {
     }
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('missing param: name'))
+  })
+})
+
+describe('Sign Up Controller', () => {
+  test('Should return 400 i no name if provider', () => {
+    // sempre comeco instancxiando a classe
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        name: 'Thiago',
+        password: '123456789',
+        passwordConfirmation: '123456789'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('missing param: email'))
   })
 })
