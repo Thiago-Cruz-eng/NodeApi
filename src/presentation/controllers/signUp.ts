d4a102b88d5bcbfc8038ba7da30ac67875282fb0
Thiago <morim240932gmail.com>
2023-07-27 20:32:05 -0300

    refactor: make signup controller implements a protocol

--- src/presentation/controllers/signUp.ts
+++ src/presentation/controllers/signUp.ts
@@ -1,8 +1,9 @@
 import { HttpResponse, HttpResquest } from '../protocols/http'
 import { MissingParamError } from '../errors/missingParamError'
 import { badRequest } from '../helpers/httpHelper'
+import { Controller } from '../protocols/controller'
 
-export class SignUpController {
+export class SignUpController implements Controller {
   handle (httpRequest: HttpResquest): HttpResponse {
     const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFields) {
