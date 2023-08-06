f2cb28c56dbdbd209e736c38e5ec3438d476993c
Thiago <morim240932gmail.com>
2023-08-06 20:50:42 -0300

    refactor: move sut creation to a factory method

--- src/presentation/controllers/signUp.spec.ts
+++ src/presentation/controllers/signUp.spec.ts
@@ -1,9 +1,13 @@
 import { SignUpController } from './signUp'
 import { MissingParamError } from '../errors/missingParamError'
 
+const makeSut = (): SignUpController => {
+  return new SignUpController()
+}
+
 describe('Sign Up Controller', () => {
   test('Should return 400 if no name is provider', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         email: 'abc@gmail.com',
@@ -17,7 +21,7 @@ describe('Sign Up Controller', () => {
   })
 
   test('Should return 400 if no email is provider', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         name: 'Thiago',
@@ -31,7 +35,7 @@ describe('Sign Up Controller', () => {
   })
 
   test('Should return 400 if no password is provider', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         email: 'abc@gmail.com',
@@ -45,7 +49,7 @@ describe('Sign Up Controller', () => {
   })
 
   test('Should return 400 if no password confirmation is provider', () => {
-    const sut = new SignUpController()
+    const sut = makeSut()
     const httpRequest = {
       body: {
         email: 'abc@gmail.com',
